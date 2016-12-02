f1c9faef91defdf4e3cec8659c831e912ae188f9
loongmxbt <loongmxbt@gmail.com>
2016-12-02 18:59:25 +0800

    static favicons

--- lib/axsofts/endpoint.ex
+++ lib/axsofts/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Axsofts.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :axsofts, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicons robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
